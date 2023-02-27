7dba4f89d3dbc77112f0c2bce0a650f835805611
GoellNico <Nico.Goell@t-online.de>
2023-02-27 12:02:25 +0100

    initialization of the project

---
+++ Movie-Manager/0-moviemanager-plugin-main/src/main/java/org/movie/manager/plugin/main/Main.java
@@ -0,0 +1,9 @@
+package src.main.java.org.movie.manager.plugin.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Start movie manager");
+        System.out.println("Stop movie manager");
+    }
+}
