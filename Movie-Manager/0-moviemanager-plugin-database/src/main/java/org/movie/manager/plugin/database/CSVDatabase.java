12e4efcae447ef99a223a1c95735f2fdbb5ed5a2
GoellNico <Nico.Goell@t-online.de>
2023-03-06 18:47:00 +0100

    add CSVDatabase and delete file from last commit

---
+++ Movie-Manager/0-moviemanager-plugin-database/src/main/java/org/movie/manager/plugin/database/CSVDatabase.java
@@ -0,0 +1,10 @@
+package org.movie.manager.plugin.database;
+
+public class CSVDatabase {
+    private final String csvPathDefault;
+    public CSVDatabase(String csvPathDefault) {
+        this.csvPathDefault= csvPathDefault;
+    }
+
+    //Need of CSV-Read and Writer Functions
+}
