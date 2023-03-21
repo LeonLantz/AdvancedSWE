df405cd6b3d2f027667e9cc5ddaa0690d80abb5e
I538913 <leon.lantz14@gmail.com>
2023-03-21 22:10:15 +0100

    create IOUtilities class
    - method for opening a container in a JFrame

---
+++ Movie-Manager/0-moviemanager-plugin-gui/src/main/java/org/movie/manager/plugin/gui/IOUtilities.java
@@ -0,0 +1,39 @@
+package org.movie.manager.plugin.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class IOUtilities {
+    public static JFrame openInJFrame(Container content, int width, int height, int posx, int posy, String title, Color bgColor, boolean exitToOS) {
+        if (content == null) {
+            throw new IllegalArgumentException("container must be given!");
+        } else {
+            final JFrame frame = new JFrame(title == null ? content.getClass().getName() : title);
+            frame.setBackground(bgColor == null ? Color.white : bgColor);
+            content.setBackground(bgColor == null ? Color.white : bgColor);
+            frame.setSize(width > 0 ? width : 100, height > 0 ? height : 100);
+            frame.setLocation(posx > 0 ? posx : 0, posy > 0 ? posy : 0);
+            frame.add(content, "Center");
+            if (exitToOS) {
+                frame.addWindowListener(new WindowAdapter() {
+                    public void windowClosing(WindowEvent event) {
+                        System.exit(0);
+                    }
+                });
+            } else {
+                frame.addWindowListener(new WindowAdapter() {
+                    public void windowClosing(WindowEvent event) {
+                        frame.setEnabled(false);
+                        frame.setVisible(false);
+                        frame.getContentPane().removeAll();
+                    }
+                });
+            }
+
+            frame.setVisible(true);
+            return frame;
+        }
+    }
+}
